bc536a489ecc0a925bcca288ac5832b4cf5969e2
lackingSemicolon <43654181+lackingSemicolon@users.noreply.github.com>
2020-07-18 01:32:31 -0700

    Attempt at using c2c transfer (FAST for big files)

--- src/TransferManager.java
+++ src/TransferManager.java
@@ -1,8 +1,10 @@
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-
-import org.apache.commons.io.FileUtils;
 
 public class TransferManager {
 	private String _src, _dest;
@@ -19,17 +21,46 @@ public class TransferManager {
 		return this._dest + relativePath;
 	}
 
-	public void transferFiles(ArrayList<Path> files) {
+	public ArrayList<Path> transferFiles(ArrayList<Path> files) {
+		int i = 1;
+		ArrayList<Path> failedTransfer = new ArrayList<Path>();
 		for (Path f : files) {
 			String newLocation = getNewFilePath(f.toString());
 			try {
-				File newPath = new File(newLocation);
-				FileUtils.copyFile(f.toFile(), newPath);
-				System.out.println("Transfered " + f.getFileName() + " to " + newLocation);
+				Path newPath = Paths.get(newLocation);
+				c2cTransfer(f, newPath); // attempt 3
+//				Files.copy(f, newPath, StandardCopyOption.REPLACE_EXISTING); // Attempt 2
+//				FileUtils.copy(f.toFile(), newPath);
+				System.out.printf("(%d/%d) Transfering %s to %s\n", i++, files.size(), f.getFileName(), newLocation);
 			} catch (Exception e) {
-				System.out.printf("Error transferring file %s to %s\n", f.toString(), newLocation);
+				System.err.printf("Error transferring file %s to %s\n", f.toString(), newLocation);
+				System.err.println("\tError msg: " + e.getMessage());
+				failedTransfer.add(f);
 			}
 		}
+		return (failedTransfer.size() == 0) ? null : failedTransfer;
+	}
+
+	// https://howtodoinjava.com/java7/nio/java-nio-2-0-how-to-transfer-copy-data-between-channels/
+	private void c2cTransfer(Path source, Path dest) throws Exception {
+		// Get channel for output file
+		FileOutputStream fos = new FileOutputStream(dest.toFile());
+		WritableByteChannel targetChannel = fos.getChannel();
+
+		// Get channel for input files
+		FileInputStream fis = new FileInputStream(source.toString());
+		FileChannel inputChannel = fis.getChannel();
+
+		// Transfer data from input channel to output channel
+		inputChannel.transferTo(0, inputChannel.size(), targetChannel);
+
+		// close the input channel
+		inputChannel.close();
+		fis.close();
+
+		// finally close the target channel
+		targetChannel.close();
+		fos.close();
 	}
 
 	private void remoteTransfer(ArrayList<Path> files) {
